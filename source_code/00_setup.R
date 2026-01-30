32851ff82fd5356fa1098fb730e670c213158fdc
Ramesh Dulal <ramesh.dulal.eco@gmail.com>
2026-01-29 23:15:17 -0600

    Update 00_setup.R

---
+++ source_code/00_setup.R
@@ -0,0 +1,14 @@
+# 00_setup.R
+# Purpose: Central configuration file
+
+# ---- EDIT THIS PATH ----
+DATA_RAW <- "/Users/rameshdulal/Library/CloudStorage/Dropbox/Nepal Civil Conflict/Results"
+
+# Where processed data can be saved inside the repo
+DATA_PROCESSED <- "data/Modified Data"
+
+if (!dir.exists(DATA_PROCESSED)) {
+  dir.create(DATA_PROCESSED, recursive = TRUE)
+}
+
+message("Setup loaded")
