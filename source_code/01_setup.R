f8d1846e5306021f6d4db18623926c1ed268b393
Ramesh Dulal <ramesh.dulal.eco@gmail.com>
2026-02-05 16:01:45 -0600

    Changed the path

--- source_code/01_setup.R
+++ source_code/01_setup.R
@@ -11,7 +11,7 @@
 
 # USER 1: Ramesh
 dropbox_path <- "/Users/rameshdulal/Library/CloudStorage/Dropbox/Nepal Civil Conflict"
-output_path <- "/Users/rameshdulal/Documents/Web Portfolio/Nepal-Civil-War-and-Int-Migration/tables"
+output_path <- "/Volumes/Ramesh-Research"
 
 # USER 2: Collaborator (uncomment and modify)
 # dropbox_path <- "Specify the dropbox path here and uncomment this line"
